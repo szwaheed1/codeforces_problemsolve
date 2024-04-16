b24a34d22b8af164d795cb4e8ff40a1f48edd05f
Syed Zahin Waheed <syedzahinwaheed@outlook.com>
2024-04-16 23:59:50 +0600

    Add files via upload

---
+++ beautiful_matrix.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+int main(){
+
+    int matrix[5][5];
+
+    for(int i=0; i < 5; i++){
+        for(int j=0; j < 5; j++){
+            cin >> matrix[i][j];
+        }
+    }
+
+    for(int i=0; i<5; i++){
+        for(int j=0; j<5; j++){
+            if(matrix[i][j] == 1){
+                cout << abs(2-i)+abs(2-j);
+            }
+        }
+    }
+
+
+    return 0;
+}
